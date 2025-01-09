347f16864170230abde58c55e968837f5bb23967
snehasherke <snehasherke92@gmail.com>
2025-01-09 19:18:36 +0530

    code with comments

--- src/test/java/com/api/test/FISassessment1.java
+++ src/test/java/com/api/test/FISassessment1.java
@@ -22,6 +22,7 @@ public class FISassessment1 {
 			String url = "https://www.ebay.com/"; 
 			
 			driver.get(url);
+			System.out.println("URL launched");
 			driver.manage().window().maximize();
 			
 			FISassessment1 f = new FISassessment1();
@@ -32,19 +33,27 @@ public class FISassessment1 {
 
 
 		public void addToCartCheck(WebDriver driver){
-			
+			//Search 'book'in search box
 			driver.findElement(By.id("gh-ac")).sendKeys("book");
 			driver.findElement(By.id("gh-btn")).click();
-	
+			
+			//click on 1st book from list
 			driver.findElement(By.xpath("(//ul[@class='srp-results srp-list clearfix'] //div[@class='s-item__title']/span[@aria-level='3'])[1]")).click();
-	
+			System.out.println("BOOK #1 initiated");
+			
+			//navigate to new tab
 			ArrayList<String> tab = new ArrayList<String>(driver.getWindowHandles());
 			driver.switchTo().window(tab.get(1));
 
+			//validate cart has 0 books
 			String cartCheck = driver.findElement(By.xpath("//li[@id='gh-minicart-hover']/div/a[1]")).getDomAttribute("aria-label");
 			Assert.assertEquals(cartCheck, "Your shopping cart");
-
+			
+			//add book to cart
 			driver.findElement(By.id("atcBtn_btn_1")).click();
+			System.out.println("Item added to cart successfully.");
+			
+			//validate 1 book is added to cart
 			String cartItems = driver.findElement(By.xpath("//*[@class='gh-cart-icon']/following-sibling::i[@id='gh-cart-n']")).getText();
 			Assert.assertEquals(1,Integer.parseInt(cartItems));
 			Assert.assertTrue(driver.findElement(By.xpath("//i[@id='gh-cart-n']")).isDisplayed());
--- src/test/java/com/api/test/FISassessment2.java
+++ src/test/java/com/api/test/FISassessment2.java
@@ -13,17 +13,23 @@ public class FISassessment2 {
 	@Test
 	 void apiTest() {
 		
+		//Fetch response for GET request
 		Response response = RestAssured.when().get("https://api.coindesk.com/v1/bpi/currentprice.json");
-		System.out.println("Response Code : "+ response.getStatusCode());
-		JsonPath jp = response.jsonPath();
-		String json = response.asString();
 		
+		//check API response is success
+		Assert.assertEquals(response.getStatusCode(), 200);
+		System.out.println("Response Code : "+ response.getStatusCode());
+		
+		JsonPath jp = response.jsonPath();
+		
+		//Validate description for GBP
 		Assert.assertEquals(jp.get("bpi.GBP.description"), "British Pound Sterling", "Correct value");
-		System.out.println(jp.getString("bpi.USD.code"));
+		
 		Assert.assertEquals(jp.getString("bpi.USD.code"), "USD", "Correct value");
 		Assert.assertEquals(jp.getString("bpi.GBP.code"), "GBP", "Correct value");
 		Assert.assertEquals(jp.getString("bpi.EUR.code"), "EUR", "Correct value");
 		
+		//Validate all 3 BPIs are present
 		 if(response.path("bpi.USD.code") == null){
 	            System.out.println("USD does not exist");
 	        }
